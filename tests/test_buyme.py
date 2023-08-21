21c6d5b422beec0ffa1cb2c588e76f936e37638a
Daniel <danielsh741@gmail.com>
2023-08-21 22:50:31 +0300

    first commit

--- tests/test_buyme.py
+++ tests/test_buyme.py
@@ -1,8 +1,11 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from pages.Login_page import LoginPage
+from pages.Selection_Page import SelectionPage
 
 
 class TestBuyMe:
@@ -31,3 +34,25 @@ class TestBuyMe:
         login = LoginPage(driver,wait)
         driver.get("https://buyme.co.il/")
         login.login("danielsh741@gmail.com","Daniel102030")
+
+        selection_page = SelectionPage(driver, wait)
+        selection_page.select_sum()
+        selection_page.select_region()
+        selection_page.select_category()
+
+        search_input = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[placeholder='איזו מתנה תרצו לחפש?']")))
+        search_input.send_keys("פרחים")
+
+        time.sleep(5)
+
+        result_element = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[value='17573218']")))
+        result_element.click()
+
+        wait.until(EC.element_to_be_clickable((By.ID, "ember1199"))).click()
+
+        time.sleep(5)
+
+        assert driver.find_element(By.CSS_SELECTOR,
+                                   "h1").text == "לא נמצאו תוצאות חיפוש ל\"רוז פרחים\" במרכז בסכום עד 99 ש\"ח בקטגוריה מתנות למזל דלי"
+
+
