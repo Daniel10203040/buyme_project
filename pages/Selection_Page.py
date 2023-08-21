b9b9389afbd7d96bc6e64e0f93fe2c3a05d63643
Daniel <danielsh741@gmail.com>
2023-08-21 22:53:21 +0300

    first commit

--- pages/Selection_Page.py
+++ pages/Selection_Page.py
@@ -9,21 +9,21 @@ class SelectionPage:
         self.driver = driver
         self.wait = wait
 
-    def select_sum():
+    def select_sum(self):
         sum_element = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[alt='סכום']")))
         sum_element.click()
         time.sleep(1)
-        sum_values = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "[value='1']")))
+        sum_values = self.wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "[value='1']")))
         sum_values[1].click()
 
-    def select_region():
+    def select_region(self):
         region_element = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[alt='אזור']")))
         region_element.click()
         time.sleep(5)
-        region_values = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "[value='11']")))
+        region_values = self.wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "[value='11']")))
         region_values[1].click()
 
-    def select_category():
+    def select_category(self):
         category_element = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[alt='קטגוריה']")))
         category_element.click()
         category_values = self.wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "[value='358']")))
