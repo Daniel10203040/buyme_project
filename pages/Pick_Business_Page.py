35a99a90eff6578795a6d054e679df72c08cdd03
Daniel <danielsh741@gmail.com>
2023-08-23 00:39:41 +0300

    first commit

--- pages/Pick_Business_Page.py
+++ pages/Pick_Business_Page.py
@@ -18,7 +18,7 @@ class PickBuisness:
         sum_element.click()
         time.sleep(3)
         sum_values = self.wait.until(EC.presence_of_all_elements_located((By.ID, "ember2498")))
-        sum_values.send_keys("100")
+        sum_values.send_keys("80")
         time.sleep(3)
         sum_element = self.wait.until(EC.element_to_be_clickable((By.ID, "ember2504")))
         sum_element.click()
